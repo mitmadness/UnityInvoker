5e6e564e9b8c65496a0244b39b9d70c2d8a4b33d
Unknown <mtouverey@methodinthemadness.eu>
2017-04-19 23:22:10 +0200

    feat(index): export logger module publicly

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as logger from './logger';
 import { IUnityOptions } from './unity_cli_options';
 import { UnityProcess } from './unity_process';
 
@@ -5,6 +6,7 @@ export { IUnityOptions } from './unity_cli_options';
 export * from './unity_finder';
 export { LinuxPlayerArch, OSXPlayerArch, WindowsPlayerArch, RendererType } from './unity_process';
 export { UnityCrashError } from './unity_invoker';
+export { logger };
 
 export function invokeUnity(options: IUnityOptions = {}): UnityProcess {
     return new UnityProcess().withOptions(options);
